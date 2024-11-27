5cd8f1d4a9715be7934b033afd3b3aa3f867226a
Harshit Agarwal <agarwalharshit2990@gmail.com>
2024-11-27 19:40:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template, send_file
 from io import BytesIO
 from PIL import Image
 from rembg import remove
+import os
 
 app = Flask(__name__)
 
@@ -18,9 +19,14 @@ def remove_background():
         image_file = request.files['image']
         image_data = image_file.read()
 
+        # Use rembg to remove the background
         output_image = remove(image_data)
         output_pil = Image.open(BytesIO(output_image))
 
+        # Optional: Resize the image to reduce memory consumption (adjust as needed)
+        output_pil = output_pil.resize((1024, 1024))
+
+        # Save image to memory buffer
         output_buffer = BytesIO()
         output_pil.save(output_buffer, format="PNG")
         output_buffer.seek(0)
@@ -30,6 +36,8 @@ def remove_background():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-port = int(os.environ.get('PORT', 10000))
+# Get the port number from the environment or default to 5000
+port = int(os.environ.get('PORT', 5000))
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
