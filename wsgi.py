65777c330694cdda06f6e164d8e824bb4d900639
Harshit Agarwal <agarwalharshit2990@gmail.com>
2024-11-30 16:13:28 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,3 @@
-   from app import app  # Replace 'my_flask_app' with the name of your Flask app
-
-   if __name__ == "__main__":
-       app.run()
+from app import app  # Replace 'my_flask_app' with the name of your Flask app
+if __name__ == "__main__":
+   app.run()
